d638268d95a15119b68fa710deca5833febe57c5
onats1 <donatemowo@yahoo.com>
2020-11-06 15:46:56 +0100

    Tested dialog view.

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 import org.junit.runner.RunWith
@@ -19,29 +20,24 @@ class MainActivityTest{
     @Test
     fun test_showDialog_captureNameInput() {
 
-        // GIVEN
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        val NAME = "Mitch"
+        ActivityScenario.launch(MainActivity::class.java)
+        val EXPECTED_NAME = "The app is pressing oniown"
 
-        // Execute and Verify
         onView(withId(R.id.button_launch_dialog)).perform(click())
 
         onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
 
         onView(withText(R.string.text_ok)).perform(click())
 
-        // make sure dialog is still visible (can't click ok without entering a name)
         onView(withText(R.string.text_enter_name)).check(matches(isDisplayed()))
 
-        // enter a name
-        onView(withId(R.id.md_input_message)).perform(typeText(NAME))
+        onView(withId(R.id.md_input_message)).perform(typeText(EXPECTED_NAME))
 
         onView(withText(R.string.text_ok)).perform(click())
 
-        // make sure dialog is gone
-        onView(withText(R.string.text_enter_name)).check(doesNotExist())
+        onView(withText(R.string.text_enter_name)).check((doesNotExist()))
 
-        onView(withId(R.id.text_name)).check(matches(withText(NAME)))
+        onView(withId(R.id.text_name)).check(matches(withText(EXPECTED_NAME)))
     }
 }
 
